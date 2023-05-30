14fcb3f89f2de47c0e1e74944d2d614058b7fba9
mintybubblegum <noa.trujillo@bosonit.com>
2023-05-30 15:56:34 +0200

    added exercise 4 from practical assessment

--- 0.PRACTICAL-ASSESMENT/exercises.js
+++ 0.PRACTICAL-ASSESMENT/exercises.js
@@ -1,4 +1,6 @@
 //exercise 1
+//Dado un array de objetos, obtener el objeto con el id 3
+
 function getId() {
     const arrNames = [
         {id: 1, name: 'Pepe'},
@@ -14,7 +16,7 @@ function getId() {
 console.log(getId());
 
 //exercise 2
-    //utilizar metodo filter
+//Dado un array de valores, devolver un array truthy (sin valores nulos, vacíos, no números, indefinidos o falsos)
     /*filter calls a function on each element of an array and returns a new array containing only the elements for which that function returns a truthy value - that is, a value which returns true if passed to the Boolean() constructor. In other words, it filters the array, based on the function passed to it. Like map, it does this without needing to modify the original array.*/
 
 function cleaningTheMessyCode() {
@@ -28,6 +30,7 @@ console.log(cleaningTheMessyCode());
 
 
 //exercise 3
+//Dado un array de ciudades, sacar todas las ciudades de España que no sean capitales
 
 function getCity() {
     const arrCities = [
@@ -46,6 +49,15 @@ function getCity() {
 console.log(getCity());
 
 //exercise 4
+//Dado tres arrays de números, sacar en un nuevo array la intersección de estos. 
+
+const arrNumber1 = [1,2,3];
+const arrNumber2 = [1,2,3,4,5];
+const arrNumber3 = [1,4,7,2];
+
+let common = arrNumber1.filter(x => arrNumber2.indexOf(x) !== -1 && arrNumber3.indexOf(x) !== -1)
+console.log("The common elements are: " + common);
+
 //exercise 5
 //exercise 6
 //exercise 7
