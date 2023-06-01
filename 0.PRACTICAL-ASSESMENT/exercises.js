7da123b800e373aef12a8916ee7da4fb8f8af5f3
mintybubblegum <noa.trujillo@bosonit.com>
2023-06-01 09:58:37 +0200

    finished practical assessment exercises no.6 & 7

--- 0.PRACTICAL-ASSESMENT/exercises.js
+++ 0.PRACTICAL-ASSESMENT/exercises.js
@@ -15,6 +15,8 @@ function getId() {
 }
 console.log(getId());
 
+//..................................................
+
 //exercise 2
 //Dado un array de valores, devolver un array truthy (sin valores nulos, vacíos, no números, indefinidos o falsos)
     /*filter calls a function on each element of an array and returns a new array containing only the elements for which that function returns a truthy value - that is, a value which returns true if passed to the Boolean() constructor. In other words, it filters the array, based on the function passed to it. Like map, it does this without needing to modify the original array.*/
@@ -28,6 +30,7 @@ function cleaningTheMessyCode() {
 }
 console.log(cleaningTheMessyCode());
 
+//..................................................
 
 //exercise 3
 //Dado un array de ciudades, sacar todas las ciudades de España que no sean capitales
@@ -48,6 +51,8 @@ function getCity() {
 }
 console.log(getCity());
 
+//..................................................
+
 //exercise 4
 //Dado tres arrays de números, sacar en un nuevo array la intersección de estos. 
 
@@ -57,6 +62,8 @@ const arrNumber3 = [1,4,7,2];
 
 let common = arrNumber1.filter(x => arrNumber2.indexOf(x) !== -1 && arrNumber3.indexOf(x) !== -1)
 console.log("The common elements are: " + common);
+
+//..................................................
 
 //exercise 5
 //Dado un array de ciudades, sacar en un nuevo array las ciudades no capitales con unos nuevos parámetros que sean city (he cambiado el nombre a notACapital para mejor comprension) y isSpain.
@@ -85,9 +92,50 @@ function getTowns(towns) {
 const resultTowns = getTowns(arrCities2); //en variable guardamos la función y le pasamos el array original 
 console.log(resultTowns); //mostramos por pantalla la variable anterior
 
+//..................................................
+
 //exercise 6
+//Crea una función que redondee un número float a un número específico de decimales. 
+//La función debe tener dos parámetros: 
+//Primer parámetro es un número float con x decimales
+//Según parámetro es un int que indique el número de decimales al que redondear
+//Evitar usar el método toFixed()
+
+function roundToTwoDecimals(num) {
+    return Math.round(num * 100) / 100;
+}
+
+console.log(roundToTwoDecimals(29.31659)); // Imprime 29.32
+
+  //1. multiplicamos el número por 100: 29.31659 * 100 = 2931.659.
+  //2. aplicamos Math.round() a este resultado para redondearlo al entero más cercano: Math.round(2931.659) = 2932.
+  //3. dividimos el número redondeado entre 100 para desplazar los dos decimales nuevamente a su posición original: 2932 / 100 = 29.32.
+
+//..................................................
+
 //exercise 7
+//Crea una función que retorne los campos de un objeto que equivalgan a un valor “falsy” después de ser ejecutados por una función específica.
+//La función debe tener dos parámetros:
+//Primer parámetro es un objeto con x número de campos y valores
+//Segundo parametro es una funcion que retorne un booleano, que se tiene que aplicar al objeto del primer parámetro
+
+
+//Ejemplo de uso de la función:
+
+function findingTheFalsy() {
+    let objNum = { a: 1, b: '2', c: 3, d: true, e: null };
+    const falsyValues = Object.entries(objNum).filter(([key, value]) => typeof value !== 'number');
+    const result = Object.fromEntries(falsyValues);
+    return result;
+}
+
+console.log(findingTheFalsy()); //{ b: '2', d: true, e: null }
+
+//..................................................
+
 //exercise 8
+
+//..................................................
 
 //exercise 9
 //Crea una función que a partir de un objeto de entrada, retorne un objeto asegurándose que las claves del objeto estén en lowercase.
@@ -115,6 +163,7 @@ function transformKeyToLowercase(obj) {
 
 console.log(objTransformed);
 
+//..................................................
 
 //exercise 10
 //Crea una función que elimine las etiquetas html o xml de un string.
@@ -130,6 +179,7 @@ console.log(tags);
 let result = removingHtmlXmlTags(tags)
 console.log(result)
 
+//..................................................
 
 //exercise 11
 //Crea una función que tome un array como parametro y lo divida en arrays nuevos con tantos elementos como sean especificados.
